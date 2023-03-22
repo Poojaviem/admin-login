62b75918cea04b675fec714de0cf663be0148c94
poojaviem <pooja_sharma@viemit.com>
2023-03-22 13:37:13 +0530

    first commit

--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -1,6 +1,9 @@
 
 import React, { useState } from 'react';
-// import {toast } from 'react-toastify';
+import {  addDoc, collection } from "firebase/firestore"; 
+import { db } from '../firebase';
+
+import {toast } from 'react-toastify';
 
 function User() {
     const [email ,setEmail] = useState()
@@ -8,26 +11,54 @@ function User() {
     const [age ,setAge] = useState()
     const [address ,setAddress] = useState()
     const [date,setDate] = useState()
-    const [aadhar,setAadhar] = useState()
+    const [phone,setPhone] = useState()
+    const [day,setDay] = useState()
     const [message,setMessage] = useState() 
 
-  const handleChange =()=>{
-     
-  }
+  const handleAdd =async(e)=>{
+    e.preventDefault()
+
+    try{
+      const res = await addDoc(collection(db, "users",), {
+        name: "abc",
+        age:"21",
+        email:"abc@gmail.com",
+        address:"rajwada",
+        phone:"7894561230",
+        date:"2/2/2023",
+        day:"123456789",
+        message:"fff",
+  
+        
+      });
+      console.log(res)
+    }
     
+
+     catch(err){
+      toast.error('something went wrong', {
+        position: toast.POSITION.TOP_CENTER
+    });
+
+     }
+  }
+   
 
     
   return (
     <div className='box'>
-    <form className='form-box' onChange={handleChange}>
-        <input type="email" placeholder='Email'value={email}  onChange={(e)=>setEmail(e.target.value)} required/><br/>
-        <input type="name" placeholder='Name' value={name} onChange={(e)=>setName(e.target.value)} required/><br/>
-        <input type="age" placeholder='Age' value={age} onChange={(e)=>setAge(e.target.value)} required/><br/>
-        <input type="address" placeholder='Address' value={address} onChange={(e)=>setAddress(e.target.value)} required/><br/>
-        <input type="date" placeholder='Date' value={date} onChange={(e)=>setDate(e.target.value)} required/><br/>
+    <form className='form-box' onSubmit={handleAdd}>
+        <input type="email" placeholder='Email'value={email}  onChange={(e)=>setEmail(e.target.value)} /><br/>
+        <input type="name" placeholder='Name' value={name} onChange={(e)=>setName(e.target.value)} /><br/>
+        <input type="phone" placeholder='Mobile no' value={phone} onChange={(e)=>setPhone(e.target.value)}/><br/>
+
+        <input type="age" placeholder='Age' value={age} onChange={(e)=>setAge(e.target.value)} /><br/>
+        <input type="address" placeholder='Address' value={address} onChange={(e)=>setAddress(e.target.value)} /><br/>
+        <input type="date" placeholder='Date' value={date} onChange={(e)=>setDate(e.target.value)} /><br/>
 
         
-        <input type="aadhar" placeholder='Aadhar no' value={aadhar} onChange={(e)=>setAadhar(e.target.value)}/><br/>
+        <input type="day" placeholder='Day' value={day} onChange={(e)=>setDay(e.target.value)}/><br/>
+
          <textarea type="message" placeholder="Message" value={message} onChange={(e)=>setMessage(e.target.value)}></textarea>      
         <button className='btn' type='submit' >Submit</button>
         
